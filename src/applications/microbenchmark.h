120d57a27648fafe08f4c6d4e707b2bb61db5523
kunrenyale <kun.ren@yale.edu>
2017-06-06 14:13:17 -0400

    test

--- src/applications/microbenchmark.h
+++ src/applications/microbenchmark.h
@@ -41,7 +41,7 @@ class Microbenchmark : public Application {
   int nparts;
   int hot_records;
   static const int kRWSetSize = 10;  // MUST BE EVEN
-  static const int kDBSize = 1000000;
+  static const int kDBSize = 10000000;
   static const int kRecordSize = 100;
 
 
