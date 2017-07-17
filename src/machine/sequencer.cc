83af1bf8ae81fb0d0a90fdc159aa2789c21d968f
renkun <renkun.umd@gmail.com>
2017-07-17 13:34:33 -0400

    minor fix

--- src/machine/sequencer.cc
+++ src/machine/sequencer.cc
@@ -71,7 +71,6 @@ void Sequencer::RunWriter() {
   uint32 txn_id_offset;
 
   uint32 local_replica = configuration_->local_replica_id();
-  uint64 local_machine = configuration_->local_node_id();
 
 /**if (configuration_->local_node_id() == 2 || configuration_->local_node_id() == 3) {
 epoch_duration_ = 0.01;
