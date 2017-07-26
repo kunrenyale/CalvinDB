37eb56be280b2d05ea6d80b7cd4774cc634f8746
renkun <renkun.umd@gmail.com>
2017-07-26 14:05:16 -0400

    debug

--- src/applications/microbenchmark.cc
+++ src/applications/microbenchmark.cc
@@ -406,7 +406,7 @@ double execution_start = GetTime();
   }
 
   // The following code is for microbenchmark "long" transaction, uncomment it if for "long" transaction
-  while (GetTime() - execution_start < 0.00004) {
+  while (GetTime() - execution_start < 0.00008) {
     int x = 1;
     for(int i = 0; i < 10000; i++) {
       x = x+10;
