5faa14387a53fece0124492aa200eb1ea3f55e2f
renkun <renkun.umd@gmail.com>
2017-07-27 11:29:22 -0400

    debug

--- src/scheduler/deterministic_scheduler.cc
+++ src/scheduler/deterministic_scheduler.cc
@@ -291,7 +291,7 @@ LOG(ERROR) << "In LockManagerThread:  After synchronization. Starting scheduler 
 
       if(done_txn->writers_size() == 0 || rand() % done_txn->writers_size() == 0)
         txns++;       
-LOG(ERROR) <<machine_id<< ":*********In LockManagerThread:  Finish executing the  txn: "<<done_txn->txn_id();
+LOG(ERROR) <<machine_id<< ":*********In LockManagerThread:  Finish executing the  txn: "<<done_txn->txn_id()<<"  origin:"<<done_txn->origin_replica();
 #ifdef LATENCY_TEST
     if (done_txn->txn_id() % SAMPLE_RATE == 0 && latency_counter < SAMPLES && done_txn->origin_machine() == machine_id) {
       scheduler_unlock[done_txn->txn_id()] = GetTime();
@@ -346,7 +346,7 @@ LOG(ERROR) <<machine_id<< ":In LockManagerThread:  got a batch(1): "<<batch_mess
 
         scheduler->lock_manager_->Lock(txn);
         pending_txns++;
-LOG(ERROR) <<machine_id<< ":In LockManagerThread:  got a txn: "<<txn->txn_id();
+LOG(ERROR) <<machine_id<< ":In LockManagerThread:  got a txn: "<<txn->txn_id()<<"  origin:"<<txn->origin_replica();
       }
     }
 
@@ -359,7 +359,7 @@ LOG(ERROR) <<machine_id<< ":In LockManagerThread:  got a txn: "<<txn->txn_id();
 
       scheduler->txns_queue->Push(txn);
 
-LOG(ERROR) <<machine_id<< ":In LockManagerThread:  Start executing the ready txn: "<<txn->txn_id();
+LOG(ERROR) <<machine_id<< ":In LockManagerThread:  Start executing the ready txn: "<<txn->txn_id()<<"  origin:"<<txn->origin_replica();
     }
 
     // Report throughput.
