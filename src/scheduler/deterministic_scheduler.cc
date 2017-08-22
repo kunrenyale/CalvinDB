39496f81cacf44eba5a9496152493c3faa3bef29
renkun <renkun.umd@gmail.com>
2017-08-22 10:14:07 -0400

    test

--- src/scheduler/deterministic_scheduler.cc
+++ src/scheduler/deterministic_scheduler.cc
@@ -460,7 +460,7 @@ LOG(ERROR) <<machine_id<< ":In LockManagerThread:  got a batch(2): "<<batch_mess
 }**/ 
     // Current batch has remaining txns, grab up to 10.
     } else if (executing_txns + pending_txns < maximum_txns) {
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 100; i++) {
         if (batch_offset >= batch_message->data_size()) {
           // Oops we ran out of txns in this batch. Stop adding txns for now.
           break;
