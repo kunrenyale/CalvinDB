767ef0d8d8b8b9b120fdb5baf01f33a865971fa0
renkun <renkun.umd@gmail.com>
2017-08-25 16:53:18 -0400

    debug

--- src/machine/client.h
+++ src/machine/client.h
@@ -103,7 +103,8 @@ class Lowlatency_MClient : public Client {
         //*txn = microbenchmark.MicroTxnMRMP(txn_id, 0, 1, 0, 1);
       } else {
         // Multi-replica single-partition txn
-        *txn = microbenchmark.MicroTxnMRSP(txn_id, replative_node_id_, local_replica_, other_replica); 
+//        *txn = microbenchmark.MicroTxnMRSP(txn_id, replative_node_id_, local_replica_, other_replica);    
+*txn = microbenchmark.MicroTxnMRSP(txn_id, 0, 0, 1);   
       }
     } else {
       // Single-replica txn.
