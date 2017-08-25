058ed739a6600ac6d2fab2c0d4780065d5a71b51
renkun <renkun.umd@gmail.com>
2017-08-25 13:48:56 -0400

    fix a bug

--- src/scheduler/deterministic_scheduler.cc
+++ src/scheduler/deterministic_scheduler.cc
@@ -548,6 +548,7 @@ LOG(ERROR) <<machine_id<< ":*********In LockManagerThread:  blocking txn: "<<txn
             // If the status is: ABORTED_WITHOUT_LOCK, we can run this txn without locking
 LOG(ERROR) <<machine_id<< ":*********In LockManagerThread:  find a  ABORTED_WITHOUT_LOCK txn: "<<txn->txn_id()<<" origin:"<<txn->origin_replica()<<"  involved_replicas:"<<txn->involved_replicas_size();
               ready_txns_->Push(txn);
+              pending_txns++;
               continue;
             } else {
               txn->set_wait_for_remaster_pros(false);
