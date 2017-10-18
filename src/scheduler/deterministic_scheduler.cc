a2c0e12ae11759ae6c386fe05f3fd96ad0bfded8
renkun <renkun.umd@gmail.com>
2017-10-18 15:50:43 -0400

    test

--- src/scheduler/deterministic_scheduler.cc
+++ src/scheduler/deterministic_scheduler.cc
@@ -360,7 +360,7 @@ LOG(ERROR) << "In LockManagerThread:  After synchronization. Starting scheduler 
   uint64 pending_txns = 0;
   int batch_offset = 0;
   uint64 machine_id = configuration_->local_node_id();
-  uint64 maximum_txns = 100;
+  uint64 maximum_txns = 1000;
   
 
   while (true) {
