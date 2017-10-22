e79c85cd04f84a1313d04b7fbaad6caa42c0976a
renkun <renkun.umd@gmail.com>
2017-10-22 16:27:57 -0400

    test

--- src/scheduler/deterministic_scheduler.cc
+++ src/scheduler/deterministic_scheduler.cc
@@ -364,7 +364,7 @@ LOG(ERROR) << "In LockManagerThread:  After synchronization. Starting scheduler 
   // For original CalvinDB high contention: Get better performance if set it smaller
   // For AsyCalvinDB with multi-replica txns: should set maximum_txns much bigger
   uint64 maximum_txns = 20000; // 10000000
-  uint64 num_txns_once = 10;
+  uint64 num_txns_once = 100;
 
   while (true) {
     TxnProto* done_txn;
