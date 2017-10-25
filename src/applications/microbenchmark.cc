a428e1063e8b31c922f9240df19bea9db5b912a6
renkun <renkun.umd@gmail.com>
2017-10-25 17:22:43 -0400

    test

--- src/applications/microbenchmark.cc
+++ src/applications/microbenchmark.cc
@@ -540,7 +540,7 @@ LOG(ERROR) <<local_replica_<< ":*********In Execute:  Generate a remaster  txn, 
   }
 
   // The following code is for microbenchmark "long" transaction, uncomment it if for "long" transaction
-  while (GetTime() - execution_start < 0.00008/factor) {
+  while (GetTime() - execution_start < 0.00012/factor) {
     int x = 1;
     for(int i = 0; i < 10000; i++) {
       x = x+10;
